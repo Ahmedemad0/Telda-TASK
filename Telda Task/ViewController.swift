0d8e640cb8596eb0f986089aab330bda143b118a
Ahmed Emad <devahmedemad1@gmail.com>
2025-10-19 02:14:36 +0300

    initial commit

---
+++ Telda Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telda Task
+//
+//  Created by Ahmed Emad on 19/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
